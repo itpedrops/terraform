6e9573c8d003f03f11b1ea25e119e00ecd5156c5
PedrOps IT <it.pedrops@gmail.com>
2018-09-06 14:44:35 -0300

    add vars to project

---
+++ aws/linux-docker-input/vars.tf
@@ -0,0 +1,12 @@
+variable "name-instance" {
+  description = "input name instance here."
+}
+variable "ami-id" {
+  description = "Input AMI-ID here."
+}
+variable "size-instance" {
+  description = "your tipe instance (t2.medium, m4.large etc)."
+}
+variable "size-ebs" {
+  description = "use number 10 to 10GB, 100 to 100GB. or another option"
+}
